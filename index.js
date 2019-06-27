1862e9ddd197d27f36016253f46d06ecd436655b
root <root@DESKTOP-GFKH6D4.localdomain>
2019-06-27 17:36:46 -0400

    Drum kit project

---
+++ index.js
@@ -0,0 +1,44 @@
+function tom1()
+{ 
+    console.log("I got clickeck")
+    let tom1 =  new Audio("./sounds/tom-1.mp3")
+    tom1.play()
+}
+function tom2()
+{ 
+    console.log("I got clickeck")
+    let tom2 =  new Audio("./sounds/tom-2.mp3")
+    tom2.play()
+}
+function tom3()
+{ 
+    console.log("I got clickeck")
+    let tom3 =  new Audio("./sounds/tom-3.mp3")
+    tom3.play()
+}
+function tom4()
+{ 
+    console.log("I got clickeck")
+    let tom4 =  new Audio("./sounds/tom-4.mp3")
+    tom4.play()
+}
+function crash()
+{ 
+    console.log("I got clickeck")
+    let crash =  new Audio("./sounds/crash.mp3")
+    crash.play()
+}
+function pedal()
+{ 
+    console.log("I got clickeck")
+    let pedal =  new Audio("./sounds/kick-bass.mp3")
+    pedal.play()
+}
+function snare()
+{ 
+    console.log("I got clickeck")
+    let snare =  new Audio("./sounds/snare.mp3")
+    snare.play()
+    event.target.classList.toggle("pressed")
+}
+
